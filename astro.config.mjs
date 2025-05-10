123c993dcfe60df702d98636bec43a73353c700b
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-05-10 16:39:15 +0400

    fix: configure github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bio.nikkoxd.ru",
   vite: {
     plugins: [tailwindcss()]
   },
